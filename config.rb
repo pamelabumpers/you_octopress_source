0be1ab83ebd95868b4194c40c7b4a8866113fc40
pamelabumpers <pamela.bumpers@aol.com>
2013-07-01 22:04:28 +0300

    init

---
+++ config.rb
@@ -0,0 +1,18 @@
+require 'sass-globbing'
+
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/you/"
+http_images_path = "/you/images"
+http_fonts_path = "/you/fonts"
+css_dir = "public/you/stylesheets"
+
+# Local development paths
+sass_dir = "sass"
+images_dir = "source/images"
+fonts_dir = "source/fonts"
+
+line_comments = false
+output_style = :compressed
